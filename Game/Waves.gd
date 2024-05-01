84c52c848e3bd4d741e1b94c4fb0490727320d34
bthaus <bwuest@gmx.at>
2024-05-01 14:23:15 +0200

    feat: add logic stubs to waves

--- Game/Waves.gd
+++ Game/Waves.gd
@@ -2,28 +2,37 @@ extends Node2D
 class_name Spawner
 @export var state:GameState
 signal wave_done
-var numMonstersActive=0;
+static var numMonstersActive=0;
+var waveMonsters=[]
+var gamestate:GameState;
 @onready var nav: NavigationAgent2D = $NavigationAgent2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	nav.target_position = $Base.global_position
 
-	
-func start(wavenumber:int):
-	var amountmonsters=10+wavenumber*3;
-	var map=state.gameBoard.get_child(0) as TileMap
-	var cells=map.get_used_cells(0);
-	amountmonsters=amountmonsters+3
-	Stats.enemy_base_HP=Stats.enemy_base_HP*1.1
-	print(Stats.enemy_base_HP)
-	numMonstersActive=amountmonsters;
-	for n in range(0,amountmonsters):
-		get_tree().create_timer(n*0.2).timeout.connect(spawnEnemy.bind(Stats.getiterativeColor(0),$Base))
 
+func start(wavenumber:int):
+	
+	doBalancingLogic(wavenumber)
+	doSpawnLogic(wavenumber)
 		
 	pass;
-func spawnEnemy(c,t):
-	var mo=Monster.create(c,t)
+func doBalancingLogic(waveNumber:int):
+	var amountmonsters=10+waveNumber*3;
+	numMonstersActive=numMonstersActive+amountmonsters;
+	for n in range(amountmonsters):
+		waveMonsters.append(Monster.create(Stats.getiterativeColor(0),$Base))
+	util.p("Im changing the stats of the minions and adding them to the array")
+	
+	pass;
+func doSpawnLogic(waveNumber:int):
+	var delay=1;
+	for mo in waveMonsters:
+		delay=delay+1;
+		get_tree().create_timer(0.2*delay).timeout.connect(spawnEnemy.bind(mo))
+	util.p("im taking minions from the array and spawn them in a sensible way")
+	pass;
+func spawnEnemy(mo:Monster):
 	mo.monster_died.connect(monsterDied)
 	mo.monster_died.connect(state.addExp)
 	add_child(mo)
