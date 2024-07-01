c1d3b0fbe96156252935e92314c53fee53f30bd9
bthaus <bwuest@gmx.at>
2024-07-01 10:38:24 +0200

    fix remove unused things

--- Cards/PlayerHand.gd
+++
@@ -1,9 +0,0 @@
-# TODO set possible cards the player can draw from here
-
-static var cardList = []
-
-static func fillCardDeck():
-	for color in Stats.TurretColor:
-		for block in Stats.BlockShape:
-			cardList.push_back(str(color,'_', block))
-			
