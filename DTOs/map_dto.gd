2b6c6eb828d7a79214028b01d2644b18898f5a92
JohannesLadurner <jo8@hotmail.de>
2024-07-03 13:43:05 +0200

    feat: added waves to mapdto, testing saving

--- DTOs/map_dto.gd
+++ DTOs/map_dto.gd
@@ -2,11 +2,14 @@ extends BaseDTO
 class_name MapDTO
 
 var entities:Array[BaseDTO]
+var waves: WavesDTO
 var map_name:String=""
 var battle_slots:BattleSlotDTO;
 
-func _init(entities: Array[BaseDTO]=[]):
+func _init(entities: Array[BaseDTO]=[], waves: WavesDTO = WavesDTO.new()):
 	self.entities = entities
+	self.waves = waves
+
 func restore(dest,acc="",dir="maps"):
 	return super.restore("map_"+dest,"",dir+"/"+dest)
 		
