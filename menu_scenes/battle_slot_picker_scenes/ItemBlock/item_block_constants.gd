0a55643ffb4eba4b3f6bf829e00778c2ed5cb30c
JohannesLadurner <jo8@hotmail.de>
2024-07-13 00:21:40 +0200

    feat: added turret containers (WIP)

--- menu_scenes/battle_slot_picker_scenes/ItemBlock/item_block_constants.gd
+++ menu_scenes/battle_slot_picker_scenes/ItemBlock/item_block_constants.gd
@@ -1,8 +1,9 @@
 extends Node
 class_name ItemBlockConstants
 
-const BLOCK_LAYER = 0
-const SELECTION_LAYER = 1
+const BACKGROUND_LAYER = 0
+const BLOCK_LAYER = 1
+const PREVIEW_LAYER = 2
 
 const GREY_TILE_ID = 0
 const GREEN_TILE_ID = 1
--- menu_scenes/battle_slot_picker_scenes/TurretContainer/turret_mod_container_item.gd
+++ menu_scenes/battle_slot_picker_scenes/TurretContainer/turret_mod_container_item.gd
@@ -1,40 +1,44 @@
 extends Panel
 
+@export var color: Stats.TurretColor
+@onready var style_box: StyleBoxFlat = get_theme_stylebox("panel").duplicate()
+
 var item_handler: ItemBlockSelectorHandler
 var selected_item: ItemBlockDTO
-@onready var item_block_container = $ItemBlockScrollContainer/ItemBlockGridContainer
+signal placed
 
 func _ready():
 	item_handler = ItemBlockSelectorHandler.new($Board, [])
-	
-	var item_blocks = [
-		ItemBlockDTO.new(Stats.TurretColor.RED, Stats.BlockShape.O, 0, ItemBlockConstants.RED_TILE_ID),
-		ItemBlockDTO.new(Stats.TurretColor.BLUE, Stats.BlockShape.O, 0, ItemBlockConstants.BLUE_TILE_ID),
-		ItemBlockDTO.new(Stats.TurretColor.GREEN, Stats.BlockShape.O, 0, ItemBlockConstants.GREEN_TILE_ID)
-	]
-	
-	for item_block in item_blocks:
-		var item = load("res://menu_scenes/battle_slot_picker_scenes/ItemBlockSelector/item_block_selector_item.tscn").instantiate()
-		item_block_container.add_child(item)
-		item.set_item_block(item_block)
-		item.clicked.connect(func(item_block): selected_item = item_block)
-		
-	
-	
-	
+	style_box.set("border_color", _get_color_from_turret_color(color))
+	add_theme_stylebox_override("panel", style_box)
+
 func _process(delta):
+	if selected_item == null: return
 	$Board.clear_layer(ItemBlockConstants.PREVIEW_LAYER)
 	var board_pos = _get_mouse_position_on_board()
 	item_handler.draw_item_block(selected_item, board_pos, ItemBlockConstants.PREVIEW_LAYER)
 	
-	
 func _unhandled_input(event):
+	if selected_item == null: return
 	if event.is_action_released("left_click"):
 		var board_pos = _get_mouse_position_on_board()
 		item_handler.place_item_block(selected_item, board_pos)
 		selected_item = null
+		placed.emit()
 	elif event.is_action_released("right_click"):
 		item_handler.rotate_item(selected_item)
 
 func _get_mouse_position_on_board() -> Vector2:
 	return $Board.local_to_map(get_global_mouse_position() / $Board.scale)
+
+func set_selected_item(item: ItemBlockDTO):
+	selected_item = item
+
+func _get_color_from_turret_color(color: Stats.TurretColor) -> Color:
+	match (color):
+		Stats.TurretColor.GREY: return Color.WEB_GRAY
+		Stats.TurretColor.GREEN: return Color.LAWN_GREEN
+		Stats.TurretColor.RED: return Color.CRIMSON
+		Stats.TurretColor.YELLOW: return Color.YELLOW
+		Stats.TurretColor.BLUE: return Color.DODGER_BLUE
+	return Color.BLACK
