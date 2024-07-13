2c011efd57c4630dfe93804446a78599b49a0680
bthaus <bwuest@gmx.at>
2024-07-13 21:53:36 +0200

    fix weird portal behaviour by overwriting abstract compute cost heuristic

--- UtilScenes/portalstar.gd
+++ UtilScenes/portalstar.gd
@@ -1,6 +1,8 @@
 extends AStar2D
 class_name PortalStar
 
-func _estimate_cost(from_id,to_id):
-	return 1
-	pass;
+func _compute_cost(from_id, to_id):
+	return 1.0
+func _estimate_cost(from_id, to_id):
+	return 1.0	
+	
