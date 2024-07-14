3ce96f8cafeec38e0867ae3ff66265efea5450bb
bthaus <bwuest@gmx.at>
2024-07-14 13:27:53 +0200

    finish stat rework

--- Game/Stats.gd
+++ Game/Stats.gd
@@ -1,7 +1,5 @@
 extends GameObject2D
 class_name Stats;
-
-
 static var rng=RandomNumberGenerator.new()
 
 enum TurretColor {WHITE=1, GREEN=2, RED=3, YELLOW=4, BLUE=5, MAGENTA=6};
@@ -9,45 +7,11 @@ enum TurretExtension {DEFAULT=1,REDLASER=2, BLUELASER=3, YELLOWMORTAR=4, GREENPO
 enum GamePhase {BATTLE=1,BUILD=2,BOTH=3};
 enum Monstertype {REGULAR=0}
 
-static var stats=Stats.new()
-
 #legacy code. keep in bc its simple af stuff, and they're basically just auxiliary stuff
 static func getStringFromEnum(type:TurretColor):
 	return Stats.TurretColor.keys()[(type)-1];
-	match type:
-		1: return "WHITE";
-		2: return "GREEN";
-		3: return "RED";
-		4: return "YELLOW";
-		5: return "BLUE"
-	pass
-static func getColorFromString(str:String):
-	match str:
-		"RED":return TurretColor.RED;
-		"BLUE":return TurretColor.BLUE;
-		"GREEN":return TurretColor.GREEN;
-		"YELLOW":return TurretColor.YELLOW;
-		"WHITE":return TurretColor.WHITE;
-		"WALL":return -3
-	pass;
-static func getEnumFromString(str:String):
-	match str:
-		"DEFAULT":return TurretExtension.DEFAULT
-		"REDLASER":return TurretExtension.REDLASER
-		"BLUELASER":return TurretExtension.BLUELASER
-		"YELLOWMORTAR":return TurretExtension.YELLOWMORTAR
-		"GREENPOISON":return TurretExtension.GREENPOISON
-		"BLUEFREEZER":return TurretExtension.BLUEFREEZER
-	pass;
 static func getStringFromEnumLowercase(type:TurretColor):
 	return Stats.TurretColor.keys()[(type)-1].to_lower();
-	match type:
-		1: return "white";
-		2: return "green";
-		3: return "red";
-		4: return "yellow";
-		5: return "blue"
-	pass
 static func getStringFromEnumExtension(type:TurretExtension):
 	match type:
 		1: return ""
@@ -81,8 +45,6 @@ static var bestCurrentAverage=1;
 
 static func getRandomCard(gamestate):
 	return BlockCard.create(gamestate)
-		
-
 	
 func getEvaluatedShape(counter):
 	var shape=Block.BlockShape.values()[rng.randi_range(0,Block.BlockShape.size()-1)]
@@ -103,5 +65,3 @@ func getEvaluatedShape(counter):
 		
 	numberOfPiecesDrawn=numberOfPiecesDrawn+block.pieces.size();	
 	return shape;	
-
-
--- Utils/color_enum.gd
+++
@@ -1,2 +0,0 @@
-class_name TurretColor
-enum {WHITE=1, GREEN=2, RED=3, YELLOW=4, BLUE=5, MAGENTA=6};
