b4307ca7aec0108e5da5c86c58b4e8c413eb9de6
JohannesLadurner <jo8@hotmail.de>
2024-07-21 16:44:57 +0200

    feat: added customizable wave number

--- LevelEditor/wave_settings.gd
+++ LevelEditor/wave_settings.gd
@@ -1,10 +1,15 @@
 extends Panel
 
 @onready var spawner_item_container = $SpawnerScrollContainer/SpawnerItemContainer
+@onready var wave_number_edit = $WaveNumber/WaveNumberEdit
+@onready var wave_number_error_label = $WaveNumber/WaveNumberErrorLabel
 
 var spawner_settings_count: int
-var wave = 0
-var numer_of_waves = 2
+var current_wave = 0
+var number_of_waves: int
+
+func _ready():
+	number_of_waves = wave_number_edit.text as int
 
 func remove_spawner_setting(spawner_id: int):
 	#Remove the spawner with the id
@@ -19,15 +24,15 @@ func remove_spawner_setting(spawner_id: int):
 			child.set_spawner_id(child.get_spawner_id() - 1)
 	
 	spawner_settings_count -= 1
-	_update_items(wave)
+	_update_items(current_wave)
 
 func add_spawner_setting():
 	var item = load("res://LevelEditor/ContainerItems/spawner_settings_item.tscn").instantiate()
 	spawner_item_container.add_child(item)
 	item.set_spawner_id(spawner_settings_count)
-	item.set_number_of_waves(numer_of_waves)
+	item.set_number_of_waves(number_of_waves)
 	spawner_settings_count += 1
-	_update_items(wave)
+	_update_items(current_wave)
 
 func _update_items(wave: int):
 	for item in spawner_item_container.get_children():
@@ -44,7 +49,7 @@ func _set_number_of_waves(amount: int):
 # v Waves
 func get_monster_waves():
 	var monster_waves = []
-	monster_waves.resize(numer_of_waves)
+	monster_waves.resize(number_of_waves)
 	for i in monster_waves.size(): monster_waves[i] = []
 	
 	for item in spawner_item_container.get_children():
@@ -58,20 +63,46 @@ func get_monster_waves():
 
 
 func _on_next_wave_button_pressed():
-	if wave < numer_of_waves - 1:
-		wave += 1
-		_set_wave_text(wave)
-		_update_items(wave)
+	if current_wave < number_of_waves - 1:
+		current_wave += 1
+		_set_current_wave(current_wave)
 
 func _on_previous_wave_button_pressed():
-	if wave > 0:
-		wave -= 1
-		_set_wave_text(wave)
-		_update_items(wave)
+	if current_wave > 0:
+		current_wave -= 1
+		_set_current_wave(current_wave)
 
-func _set_wave_text(wave: int):
-	$WaveLabel.text = str("Wave: ", wave)
+func _set_current_wave(wave: int):
+	$WaveLabel.text = str("Wave: ", wave+1)
+	_update_items(wave)
+
+
+func _on_wave_number_set_button_pressed():
+	wave_number_error_label.visible = false
+	var waves_text = wave_number_edit.text.strip_edges()
+	var regex = RegEx.new()
+	regex.compile("\\d+")
+	var result = regex.search(waves_text)
+	if not result or result.get_string() != waves_text:
+		wave_number_error_label.text = "Invalid Input!"
+		wave_number_error_label.visible = true
+		wave_number_edit.text = str(number_of_waves) #Reset to original value
+		return
+	
+	var waves = waves_text as int
+	if waves < GameplayConstants.MIN_NUMBER_OF_WAVES or waves > GameplayConstants.MAX_NUMBER_OF_WAVES:
+		wave_number_error_label.text = str("Must be in range ", GameplayConstants.MIN_NUMBER_OF_WAVES, " - ", GameplayConstants.MAX_NUMBER_OF_WAVES)
+		wave_number_error_label.visible = true
+		wave_number_edit.text = str(number_of_waves) #Reset to original value
+		return
+
+	#Update waves
+	if current_wave > waves: #If we edit wave 10 for example but now only have 5 anymore, set current_wave to 5.
+		current_wave = waves - 1
+		_set_current_wave(current_wave)
+		
+	number_of_waves = waves
+	_set_number_of_waves(number_of_waves)
 
 func _on_close_button_pressed():
 	hide()
-
--- Utils/gameplay_constants.gd
+++ Utils/gameplay_constants.gd
@@ -5,6 +5,9 @@ const CAMERA_MOVE_DURATION = 2
 const MAX_TURRET_LEVEL = 5
 const CARD_PLACEMENT_DELAY = 0.2
 
+const MIN_NUMBER_OF_WAVES = 1
+const MAX_NUMBER_OF_WAVES = 100
+
 const green_explosion_range=0.5;
 const red_laser_damage_stack=3;
 const green_poison_damage_stack=6;
