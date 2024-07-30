9333ff2d4375b7f7fbfbb30c4588ec97ae4ac226
bthaus <bwuest@gmx.at>
2024-07-30 16:29:33 +0200

    add poison

--- TurretMods/poison_ammo.gd
+++ TurretMods/poison_ammo.gd
@@ -1,3 +1,14 @@
 extends TurretBaseMod
 class_name PoisonAmmunitionMod
 
+func on_hit(p:Projectile,m:Monster):
+	if !util.valid(m):return
+	var poison=PoisonDebuff.new(POISON_AMMO_FIRE_DURATION*POISON_AMMO_SCALING*level,
+	associate
+	,level,
+	POISON_AMMO_TICK_DAMAGE*FIRE_AMMO_SCALING*level,
+	POISON_AMMO_PROPAGATION_TIME*POISON_AMMO_SCALING*level)
+	poison.register(m)
+	super(p,m)
+	pass;
+
