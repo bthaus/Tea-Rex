10da77ef0b75109fc11fabb2c8b290dad45914c4
bthaus <bwuest@gmx.at>
2024-08-04 12:36:26 +0200

    start entity factory

--- GameBoard/Entities/volcano_entity.gd
+++ GameBoard/Entities/volcano_entity.gd
@@ -1,3 +1,34 @@
 extends BaseEntity
-class_name VolcanoEntity
+class_name BurningVolcanoEntity
 
+@export var explosion_time_min:float=4
+@export var explosion_time_max:float=12
+
+@export var fire_damage:float=10
+@export var fire_duration:float=4
+
+var explosion_timer=0
+var running=false
+func on_battle_phase_started():
+	running=true
+	set_timer()
+	pass;
+func set_timer():
+	explosion_timer=randf_range(explosion_time_min,explosion_time_max)
+	pass;	
+func on_build_phase_started():
+	running=false
+	pass;	
+func do(delta):
+	if not running:return
+	explosion_timer-=delta
+	if explosion_timer<0:
+		trigger_volcano()
+		set_timer()
+		
+	
+	pass;
+
+func trigger_volcano():
+	
+	pass;
---
+++ factories/entity_factory.gd
@@ -0,0 +1,6 @@
+class_name EntityFactory
+
+
+func create(dto:EntityDTO):
+	
+	pass;
