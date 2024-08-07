bff01bc3f5f52132b88aef66d892a772ddfff337
bthaus <bwuest@gmx.at>
2024-08-07 16:51:02 +0200

    add remove to spielverderber

--- factories/Spielverderber.gd
+++ factories/Spielverderber.gd
@@ -10,3 +10,10 @@ func place_on_board(board: TileMap):
 
 	
 	pass;
+func remove_from_board(board:TileMap):
+	var vecs=[]
+	GameState.gameState.collisionReference.getNeighbours(get_global(),vecs)
+	for v in vecs:
+		GameState.collisionReference.remove_entity_from_position(self,board.map_to_local(v))
+
+	pass;
