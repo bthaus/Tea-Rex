064d987105a2404b0f1b6833baff0a649060618b
bthaus <bwuest@gmx.at>
2024-08-07 17:22:57 +0200

    add laserstub, start new spielverderber

--- factories/Laser.gd
+++ factories/Laser.gd
@@ -1,5 +1,19 @@
 extends BaseEntity
 class_name LaserEntity
+@export var shoot_min_time:float=1
+@export var shoot_max_time:float=10
+@export var direction:Direction=Direction.LEFT
 
+enum Direction{LEFT,RIGHT,UP,DOWN}
 
+func _ready():
+	
+	get_tree().create_timer(get_random_time()).timeout.connect(shoot_laser)
+	pass;
 
+func get_random_time():
+	return randf_range(shoot_min_time,shoot_max_time)
+	
+func shoot_laser():
+	
+	pass;
--- factories/Spielverderber.gd
+++ factories/Spielverderber.gd
@@ -4,6 +4,7 @@ class_name SpielverderberEntity
 func place_on_board(board: TileMap):
 	super(board)
 	var vecs=[]
+	var dto=EntityDTO.new()
 	GameState.gameState.collisionReference.getNeighbours(get_global(),vecs)
 	for v in vecs:
 		GameState.collisionReference.register_entity_at_position(self,board.map_to_local(v))
