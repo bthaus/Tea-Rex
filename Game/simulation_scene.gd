353d11aa41da818f1b80a0ed202ed7f63427dca8
bthaus <bwuest@gmx.at>
2024-09-05 18:46:19 +0200

    sahelf

--- Game/simulation_scene.gd
+++ Game/simulation_scene.gd
@@ -4,7 +4,7 @@ class_name SimulationState
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	startGame()
-	board.hide()
+	#board.hide()
 	pass # Replace with function body.
 
 
--- menu_scenes/battle_slot_picker_scenes/battle_slot_picker.gd
+++ menu_scenes/battle_slot_picker_scenes/battle_slot_picker.gd
@@ -27,15 +27,15 @@ func enable_sandbox_mode():
 
 
 func _on_button_pressed():
-	$Polygon2D/turret_preview_scene.queue_free()
+	preview.queue_free()
 	var game_state = MainMenu.get_scene_instance(MainMenu.GAME_STATE_PATH)
 	game_state.map_dto = map
 	game_state.register_battle_slot_containers($BlockSelector.selected_containers)
 	MainMenu.change_content(game_state)
 
-
+var preview
 func _on_tree_entered():
-	var preview=load("res://menu_scenes/turret_preview/Turret_preview_scene.tscn").instantiate() as Simulation
+	preview=load("res://menu_scenes/turret_preview/Turret_preview_scene.tscn").instantiate() as Simulation
 	preview.map_name=preview_map_name
 	preview.turret_position=preview_map_turret_position
 	$preview_holder.add_child(preview)
