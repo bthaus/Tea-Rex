48298f20dff5b86ef1e6ecce1da09b0d47ad447b
JohannesLadurner <jo8@hotmail.de>
2024-09-06 14:15:52 +0200

    feat: added path that shows unlocked levels

--- DTOs/map_status_dto.gd
+++ DTOs/map_status_dto.gd
@@ -3,8 +3,9 @@ class_name MapStatusDTO
 
 var map_name
 var stars_unlocked
+var unlocked: bool
 
-func _init(name="",unlocks=0):
-	map_name=name
-	stars_unlocked=unlocks
-	pass;
+func _init(name = "", stars_unlocked = 0, unlocked: bool = false):
+	self.map_name = name
+	self.stars_unlocked = stars_unlocked
+	self.unlocked = unlocked
--- menu_scenes/LevelSelection/level_selection.gd
+++ menu_scenes/LevelSelection/level_selection.gd
@@ -3,11 +3,15 @@ extends Node2D
 @onready var level_rows_container = $ScrollContainer/MarginContainer/LevelRowsContainer
 const LEVEL_COLUMNS = 6
 const ITEM_SEPERATION = 75
-
+var level_names: Array
+var path_points: Array
 
 func _ready():
 	level_rows_container.add_theme_constant_override("separation", ITEM_SEPERATION)
 	set_levels("")
+	await get_tree().process_frame
+	set_paths()
+	queue_redraw()
 
 func set_levels(chapter_name: String):
 	
@@ -18,7 +22,7 @@ func set_levels(chapter_name: String):
 	#var chapters=MapChapterDTO.new()
 	#chapters.restore()
 	#var level_names = chapters.get_mapnames_from_chapter(chapter_name)
-	var level_names = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+	level_names = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"]
 	var idx = 0
 	var row = -1
 	for level in level_names:
@@ -35,3 +39,42 @@ func set_levels(chapter_name: String):
 		if row % 2 == 1:
 			level_rows_container.get_child(row).move_child(item, 0) #Reverse order for odd-numbered rows
 		idx += 1
+
+func set_paths():
+	var row = 0
+	var idx = 0
+	path_points.clear()
+	for container in level_rows_container.get_children():
+		var items = container.get_children()
+		if row % 2 == 1: items.reverse()
+		var col = 0
+		for item in items:
+			if not item.unlocked: #This was the last level that is unlocked
+				return
+			idx += 1
+			if level_names.size() == idx: #It is the last level
+				return
+			
+			var next_item
+			if col < items.size() - 1:
+				next_item = items[col + 1]
+			else:
+				var cont = level_rows_container.get_child(row+1)
+				if (row + 1) % 2 == 0: next_item = cont.get_child(0) #First item
+				else: next_item = cont.get_child(cont.get_child_count()-1) #Last item
+			
+			path_points.append(Connection.new(item.global_position + item.size/2, next_item.global_position + next_item.size/2))
+			col += 1
+			
+		row += 1
+
+func _draw():
+	for point in path_points:
+		draw_line(point.from, point.to, Color.WHITE, 2.0, true)
+
+class Connection:
+	var from: Vector2
+	var to: Vector2
+	func _init(from: Vector2, to: Vector2):
+		self.from = from
+		self.to = to
