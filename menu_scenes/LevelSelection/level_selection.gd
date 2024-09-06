39d810e93b2f68c8709c237991a0052748ecdb9c
JohannesLadurner <jo8@hotmail.de>
2024-09-07 00:14:27 +0200

    feat: added dashed lines for path

--- menu_scenes/LevelSelection/level_selection.gd
+++ menu_scenes/LevelSelection/level_selection.gd
@@ -4,14 +4,14 @@ extends Node2D
 const LEVEL_COLUMNS = 6
 const ITEM_SEPERATION = 75
 var level_names: Array
-var path_points: Array
 
 func _ready():
 	level_rows_container.add_theme_constant_override("separation", ITEM_SEPERATION)
 	set_levels("")
 	await get_tree().process_frame
-	set_paths()
-	queue_redraw()
+	var paths = get_paths()
+	for path in paths:
+		draw_path(path, 3, 20, 10)
 
 func set_levels(chapter_name: String):
 	
@@ -35,25 +35,27 @@ func set_levels(chapter_name: String):
 		
 		var item = load("res://menu_scenes/LevelSelection/level_selection_item.tscn").instantiate()
 		item.set_level(level)
+		if idx >= 10:
+			item.unlocked = false
 		level_rows_container.get_child(row).add_child(item)
 		if row % 2 == 1:
 			level_rows_container.get_child(row).move_child(item, 0) #Reverse order for odd-numbered rows
 		idx += 1
 
-func set_paths():
+func get_paths() -> Array[Path]:
 	var row = 0
 	var idx = 0
-	path_points.clear()
+	var path_points: Array[Path] = []
 	for container in level_rows_container.get_children():
 		var items = container.get_children()
 		if row % 2 == 1: items.reverse()
 		var col = 0
 		for item in items:
 			if not item.unlocked: #This was the last level that is unlocked
-				return
+				return path_points
 			idx += 1
 			if level_names.size() == idx: #It is the last level
-				return
+				return path_points
 			
 			var next_item
 			if col < items.size() - 1:
@@ -63,16 +65,37 @@ func set_paths():
 				if (row + 1) % 2 == 0: next_item = cont.get_child(0) #First item
 				else: next_item = cont.get_child(cont.get_child_count()-1) #Last item
 			
-			path_points.append(Connection.new(item.global_position + item.size/2, next_item.global_position + next_item.size/2))
+			path_points.append(Path.new(item.global_position + item.size/2, next_item.global_position + next_item.size/2))
 			col += 1
 			
 		row += 1
+	return path_points
 
-func _draw():
-	for point in path_points:
-		draw_line(point.from, point.to, Color.WHITE, 2.0, true)
+#https://math.stackexchange.com/questions/175896/finding-a-point-along-a-line-a-certain-distance-away-from-another-point
+func draw_path(path: Path, number_of_dashes: int, space: int, width: int = 5.0):
+	var path_length = sqrt(pow(path.to.x - path.from.x, 2) + pow(path.to.y - path.from.y, 2))
+	var dash_length = (path_length - ((number_of_dashes - 1) * space)) / number_of_dashes
+	
+	var previous_point = path.from
+	var total_distance = dash_length
+	for dash in number_of_dashes:
+		var t = total_distance/path_length #distance ratio
+		var point = Vector2((1-t)*path.from.x + t*path.to.x, (1-t)*path.from.y + t*path.to.y)
+		
+		var line = Line2D.new()
+		line.add_point(previous_point)
+		line.add_point(point)
+		line.begin_cap_mode = Line2D.LINE_CAP_ROUND
+		line.end_cap_mode = Line2D.LINE_CAP_ROUND
+		line.width = width
+		add_child(line)
+		
+		total_distance += space #Add the space
+		t = total_distance/path_length #distance ratio
+		previous_point = Vector2((1-t)*path.from.x + t*path.to.x, (1-t)*path.from.y + t*path.to.y)
+		total_distance += dash_length #Add the next dash distance
 
-class Connection:
+class Path:
 	var from: Vector2
 	var to: Vector2
 	func _init(from: Vector2, to: Vector2):
