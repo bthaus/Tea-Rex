71a2427e9ba27996a1b7419305f334512b7fcc75
JohannesLadurner <jo8@hotmail.de>
2024-09-06 10:15:20 +0200

    feat: moved simulation to block selector

--- menu_scenes/battle_slot_picker_scenes/battle_slot_picker.gd
+++ menu_scenes/battle_slot_picker_scenes/battle_slot_picker.gd
@@ -2,7 +2,7 @@ extends GameObject2D
 class_name BattleSlotPicker
 var map_name
 var map
-var preview
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var map_dto=MapDTO.new()
@@ -24,22 +24,8 @@ func _process(delta):
 func enable_sandbox_mode():
 	$BlockSelector.enable_sandbox_mode()
 
-var preview_map_name="preview_map"
-var preview_map_turret_position=Vector2(9,6)
 func _on_start_button_pressed():
-	preview.free()
 	var game_state = MainMenu.get_scene_instance(MainMenu.GAME_STATE_PATH)
 	game_state.map_dto = map
 	game_state.register_battle_slot_containers($BlockSelector.selected_containers)
 	MainMenu.change_content(game_state)
-func _on_tree_entered():
-	preview=load("res://menu_scenes/turret_preview/Turret_preview_scene.tscn").instantiate() as Simulation
-	preview.map_name=preview_map_name
-	preview.auto_start=false
-	preview.turret_position=preview_map_turret_position
-	$preview_holder.add_child(preview)
-
-
-func _on_tree_exited():
-	util.erase(preview)
-	pass # Replace with function body.
