5a2992db5aa1460739f6d14cdf26b8d2c2bce72c
JohannesLadurner <jo8@hotmail.de>
2024-09-11 12:49:03 +0200

    feat: moved level naming outside of editor

--- menu_scenes/LevelEditor/Menu/level_editor_menu.gd
+++ menu_scenes/LevelEditor/Menu/level_editor_menu.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 @onready var level_container = $LevelScrollContainer/LevelVBoxContainer
+@onready var level_name = $NewLevel/LevelName
 
 
 func _ready():
+	$NewLevel.hide()
 	for item in level_container.get_children():
 		item.queue_free()
 	
@@ -20,4 +22,12 @@ func _ready():
 
 
 func _on_new_level_button_pressed(): 
-	MainMenu.change_content(MainMenu.get_scene_instance(MainMenu.LEVEL_EDITOR_PATH))
+	$NewLevel.show()
+
+func _on_create_button_pressed():
+	var level_editor = MainMenu.get_scene_instance(MainMenu.LEVEL_EDITOR_PATH)
+	level_editor.map_name = level_name.text
+	MainMenu.change_content(level_editor)
+
+func _on_cancel_button_pressed():
+	$NewLevel.hide()
