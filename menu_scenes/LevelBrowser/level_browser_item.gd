4bd650af53bc9c751a6dee5c372f04bf40e696ba
JohannesLadurner <80148992+JohannesLadurner@users.noreply.github.com>
2024-09-27 13:52:55 +0200

    fix: replaced rating with likes

--- menu_scenes/LevelBrowser/level_browser_item.gd
+++ menu_scenes/LevelBrowser/level_browser_item.gd
@@ -1,5 +1,10 @@
 extends Panel
 
+var _map_dto: MapDTO
+
+func set_level(map_dto: MapDTO):
+	self._map_dto = map_dto
+	$MapNameLabel.text = map_dto.map_name
 
 func _on_view_button_pressed() -> void:
-	pass # Replace with function body.
+	pass # Replace
