916b205d904e59ce669bf18dbb732c88cff75536
bthaus <bwuest@gmx.at>
2024-09-27 10:54:22 +0200

    add multiportshaders

--- multiviewport.gd
+++ multiviewport.gd
@@ -6,23 +6,27 @@ class_name MultiViewPort
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var nodes:Array[PortNode]=[]
+	
 	var previous_pass:Sprite2D
 	for shader in shader_array:
 		var node=create_port_node(shader)
 		nodes.append(node)
-	var last_pass=nodes.pop_back() as PortNode
-	last_pass.sprite.texture=texture
-	var first_pass=last_pass
-	for node:PortNode in nodes:
-		node.sprite.texture=last_pass.viewport.get_texture()
-		last_pass=node
-	texture=last_pass.viewport.get_texture()	
-	nodes.reverse()
-	nodes.push_back(first_pass)
 	var root=self
 	for node:PortNode in nodes:
-		root.add_child(node.viewport)
-		root=node.viewport
+		add_child(node.viewport)
+		#root.viewport.add_child(node.viewport)
+		root=node
+	root.sprite.texture=texture
+	var leaf=root;
+	nodes.erase(leaf)
+	nodes.reverse()
+	for node in nodes:
+		node.sprite.texture=leaf.viewport.get_texture()
+		leaf=node
+	texture=leaf.viewport.get_texture()	
+
+	
+	
 		
 	pass # Replace with function body.
 func create_port_node(shader)->PortNode:
